e754d0ae20e890874a1deb8c706d3161fd8a22ff
divinejoshua <divineuche399@gmail.com>
2023-04-10 14:42:34 +0100

    Added data to inline images

--- src/app/shared/inlineimages/inlineimages.component.ts
+++ src/app/shared/inlineimages/inlineimages.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-inlineimages',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./inlineimages.component.css']
 })
 export class InlineimagesComponent {
+  @Input() inlineImages: any;
 
 }
