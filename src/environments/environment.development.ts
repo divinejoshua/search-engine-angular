741ad094793bada72ea6008a512474360b01881b
divinejoshua <divineuche399@gmail.com>
2023-04-10 15:55:54 +0100

    Fixed answerbox errors

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  domain: "https://api.scaleserp.com/search",
+  domain: "/assets/data/results.json",
   apiKey: "C2989165E0D84615BEF2DDFD73512DAF",
 
 };
