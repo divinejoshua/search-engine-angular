fac47976999ca00f17ca497d7f882c4edf1a5331
divinejoshua <divineuche399@gmail.com>
2023-04-10 14:11:55 +0100

    Completed result list component

--- src/app/shared/resultlist/resultlist.component.ts
+++ src/app/shared/resultlist/resultlist.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-resultlist',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class ResultlistComponent {
 
+  @Input() results: any;
+
+  constructor(){
+      this.results = []
+  }
+
+  ngOnInit() {
+  }
 }
