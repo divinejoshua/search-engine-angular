5863f1f99ca4944d6f0d03a9709d3d230874f635
divinejoshua <divineuche399@gmail.com>
2023-04-10 14:20:13 +0100

    Adde production environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  production: false,
   domain: "/assets/data/result.json",
   apiKey: "C2989165E0D84615BEF2DDFD73512DAF",
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  production: true,
   domain : "https://api.scaleserp.com/search",
   apiKey: "C2989165E0D84615BEF2DDFD73512DAF",
 };
